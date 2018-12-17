434b9a4f8c75c4ecae9713e84b0b42265407cc7d
rtvb5 <rtvb5@reasol.lan>
2018-12-17 19:45:07 +0800

    jj

--- src/hero/Hero.java
+++ src/hero/Hero.java
@@ -10,27 +10,33 @@ public class Hero {
 	int kill;// 杀人次数
 	int golden;// 金钱
 
+	//坑队友
 	void keng() {
 		System.out.println("坑队友！");
 
 	}
 
+	//超神
 	void legendary() {
 		System.out.println("超神");
 	}
 
+	//获取血量
 	float getHp() {
 		return hp;
 	}
 
+	//回血
 	void rec(float blood) {
 		hp = hp + blood;
 	}
 
+	//获取移速
 	int getMoveSpeed() {
 		return moveSpeed;
 	}
 
+	//增加金钱
 	void addGolden(int coin) {
 		golden = golden + coin;
 	}
@@ -44,6 +50,15 @@ public class Hero {
 		garen.died = 100;
 		garen.kill = 4;
 		garen.golden = 500;
+		
+		Hero wangpipi = new Hero();
+		wangpipi.name = "汪冰桢";
+		wangpipi.hp = 100;
+		wangpipi.moveSpeed = 250;
+		wangpipi.hujia = 10000;
+		wangpipi.died = 9;
+		wangpipi.kill =999999;
+		wangpipi.golden =1000000;
 
 		System.out.print(garen.name);
 		System.out.println("血量:" + garen.hp);
@@ -53,8 +68,10 @@ public class Hero {
 		System.out.println("增加1000");
 		garen.addGolden(1000);
 		System.out.println("加钱后：" + garen.golden);
-	    System.out.println(garen.kill);
-	    System.out.println("改下试试");
+	  //  System.out.println(garen.kill);
+	//    System.out.println("改下试试");
+	    
+	    System.out.println("姓名：" + wangpipi.name + "  血量：" + wangpipi.hp + "   护甲" + wangpipi.hujia);
 
 	}
 
